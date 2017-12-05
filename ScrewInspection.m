9661b94ec4a5f6225d0ac047d6e0067924c1a32f
caochao39 <caochao39@gmail.com>
2017-12-04 20:55:07 -0500

    add visualization & change result to struct array

--- ScrewInspection.m
+++ ScrewInspection.m
@@ -1,5 +1,11 @@
 function [result] = ScrewInspection(imgpath, visualization)
 
+    if nargin < 2
+      visualization = false;
+    end
+
+    result = struct;
+
     length_std = 68;
     width_std = 39;
     length_thr = 7;
@@ -13,6 +19,13 @@ function [result] = ScrewInspection(imgpath, visualization)
     load('contour_template.mat');
 
     img = imread(imgpath); img = imrotate(img,-90);img = imresize(img,0.1);
+
+    if visualization == true
+      figure;
+      subplot(2, 3, 1);
+      imshow(img);
+    end
+
     [line,im_patch,bw_patch] = findScrew(img);
     [boundary] = extractBoundary(bw_patch);
     contour = [boundary(:,2),boundary(:,1)];
@@ -51,40 +64,60 @@ function [result] = ScrewInspection(imgpath, visualization)
 %         plot(contour(:,1),contour(:,2));
     end
 
+
 % check screw length and width
     length_err = length_scale-length_std;
     if abs(length_err) > length_thr
     	if length_err>0
-    		result = 'too long';
-    		return;
+        result.length = 'too long';
     	else
-    		result = 'too short';
-    		return;
+        result.length = 'too short';
     	end
+    else
+      result.length = 'good';
     end
+
+
 
     width_err = width_scale-width_std;
     if abs(width_err) > width_thr
     	if width_err>0
-    		result = 'too fat';
-    		return;
+        result.width = 'too fat';
     	else
-    		result = 'too skinny';
-    		return;
+        result.width = 'too skinny';
     	end
+    else
+      result.width = 'good';
+    end
+
+
+    if visualization == true
+      subplot(2, 3, 2);
+      imshow(bw_patch);
+      hold on;
+      plot(contour(:,1), contour(:,2), 'LineWidth', 2, 'Color', 'g');
+      xlabel({['length: ', num2str(length_scale)],['width: ', num2str(width_scale)]});
     end
 
     % check screw head
     bw_head = bw_patch(1:20,:);
     [bound_head] = extractBoundary(bw_head);
+
     head_area = polyarea(bound_head(:,2),bound_head(:,1));
     if head_area>1200
-        result = 'big head';
-        return;
+      result.head = 'too big';
+    elseif head_area<900
+      result.head = 'too small';
+    else
+      result.head = 'good';
     end
-    if head_area<900
-        result = 'small head';
-        return;
+
+    if visualization == true
+      subplot(2, 3, 3);
+      imshow(bw_head);
+      hold on;
+      plot(bound_head(:,2), bound_head(:,1), 'LineWidth', 2, 'Color', 'g');
+      xlabel({['head area: ', num2str(head_area)]; ['result: ', result.head]});
     end
 
     % check screw thread
@@ -92,13 +125,29 @@ function [result] = ScrewInspection(imgpath, visualization)
     thread_part = [10 138];
     contour(contour(:,2)>thread_part(2)|contour(:,2)<thread_part(1),:)=[];
     [in_l, in_r, out_l, out_r] = findExtrema(contour);
+
+    if visualization == true
+      subplot(2, 3, 4);
+      plot(contour(:,1), contour(:,2));
+      hold on;
+      scatter(in_l(:,1), in_l(:,2), 'g','filled');
+      scatter(in_r(:,1), in_r(:,2), 'g','filled');
+      scatter(out_l(:,1), out_l(:,2), 'r','filled');
+      scatter(out_r(:,1), out_r(:,2), 'r','filled');
+    end
     % need to be improved\
 
 
     if min([size(in_l,1),size(in_r,1),size(out_l,1),size(out_r,1)])<thread_num
-    	result = 'thread missing';
-    	return;
-	end
+    	% result = 'thread missing';
+    	% return;
+      result.thread_num = 'thread missing';
+    else
+      result.thread_num = 'good';
+	   end
+
+
+
 
 % 	[inner_matches,~] = threadpointMatch(in_l,in_r,thread_num);
 % 	[out_matches,~] = threadpointMatch(out_l,out_r,thread_num);
@@ -114,15 +163,46 @@ function [result] = ScrewInspection(imgpath, visualization)
 	[thread_l,~] = threadpointMatch(in_l,out_l,thread_num);
 	[thread_r,~] = threadpointMatch(in_r,out_r,thread_num);
 
+  if visualization == true
+    subplot(2, 3, 5);
+    plot(contour(:,1), contour(:,2));
+    hold on;
+    scatter(in_l(:,1), in_l(:,2), 'g','filled');
+    scatter(in_r(:,1), in_r(:,2), 'g','filled');
+    scatter(out_l(:,1), out_l(:,2), 'r','filled');
+    scatter(out_r(:,1), out_r(:,2), 'r','filled');
+    for i = 1 : length(thread_l)
+      plot([in_l(thread_l(i,1),1), out_l(thread_l(i,2),1)], [in_l(thread_l(i,1),2), out_l(thread_l(i,2),2)], 'LineWidth', 2);
+    end
+
+    for i = 1 : length(thread_r)
+      plot([in_r(thread_r(i,1),1), out_r(thread_r(i,2),1)], [in_r(thread_r(i,1),2), out_r(thread_r(i,2),2)], 'LineWidth', 2);
+    end
+
+  end
+
+
+  % TODO: compute distances from extreme points to the ransac line
 	left_d = abs(in_l(thread_l(:,1),1) - out_l(thread_l(:,2),1));
 	right_d = abs(in_r(thread_r(:,1),1) - out_r(thread_r(:,2),1));
 
 	if sum(abs(left_d(3:end-1) - thread_h)>thread_h_thr)>1 || sum(abs(right_d(3:end-1) - thread_h)>thread_h_thr)>1
-		result = 'problem screw thread';
-		return;
+		% result = 'problem screw thread';
+		% return;
+    result.thread_height = 'problematic';
+  else
+    result.thread_height = 'good';
 	end
 
-	result = 'good screw';
 
+  fields = fieldnames(result);
+  for i = 1:numel(fields)
+    if ~strcmp(result.(fields{i}), 'good')
+      result.overall = 'bad screw';
+      return;
+    end
+  end
+
+  result.overall = 'good screw';
 
 end
--- ransacLine.m
+++ ransacLine.m
@@ -1,22 +1,22 @@
 function [best_inliers] = ransacLine(pts)
 
 N = size(pts,1);
-err = 2.5;
+err = 2;
 best_num = 0;
 
 % for i=1:nIter
 %     x1 = pts(i,1); y1 = pts(i,2);
-%     
+%
 %     dis = abs(pts(:,1)-x1);
 %     inliers = find(dis<err);
 %     num = numel(inliers);
-%     
+%
 %     if best_num < num
 %         best_num = num;
 %         best_inliers = inliers;
 %     end
 % end
-% 
+%
 % x = pts(best_inliers,1);
 % y = pts(best_inliers,2);
 % c = pinv([x,ones(size(x))])*y;
@@ -29,10 +29,10 @@ for i=1:20
     x = pts(r,1);y = pts(r,2);
     c = pinv([x,ones(size(x))])*y;
     d = abs(c(1)*pts(:,1) -pts(:,2)+c(2))/sqrt(c(1)^2+1);
-    
+
     inliers = find(d<err);
     num = numel(inliers);
-    
+
     if best_num < num
         best_num = num;
         best_inliers = inliers;
@@ -42,9 +42,8 @@ end
 %     X = [pts(best_inliers,1),ones(size(Y))];
 %     c = pinv(X)*Y;
 %     A = c(1); C = c(2);
-%     
+%
 %     %avg dis of inliers
 %     avg_dis = sum(abs((c(1)*X_r+c(2)*Y_r+c(3)-Z_r))/sqrt(c(1)^2+c(2)^2+1))/numel(Z_r);
-    
-end
 
+end
